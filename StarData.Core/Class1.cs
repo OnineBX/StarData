23e567a0ab7fb0ddb62c18e9530cb544f8aebc96
RyanXu <hitxcl@gmail.com>
2021-06-21 11:07:13 +1200

    init project

---
+++ StarData.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StarData.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ StarData.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace StarData.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
